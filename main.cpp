212e1b2f7ec3b491dffcd22483a64aea394cd9f4
selvphiel <Tymofii.Hatilov@lnu.edu.ua>
2025-05-07 12:26:16 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+#include <iostream>
+
+int main() {
+	std::cout << "hello, world!" << std::endl;
+	return 0;
+}
+=======
+#include <iostream>
+
+int main() {
+	std::cout << "hello, world!" << std::endl;
+	return 0;
+}
+>>>>>>> 96c3fe8 (Initial commit)
